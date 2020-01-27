1ad4407043fc33d0e241367cfce819125ab9df64
nitairoy <nitaishapla@gmail.com>
2020-01-27 20:12:06 +0100

    Heroku cofig files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l)@d2-&h_lgd@m!$^7^nr1o(ixezqmm^kje*!-r=$b_+(ot*eo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
